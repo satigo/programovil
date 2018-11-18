2c849d2fbfd6917db0edfd75cb523d7bd6a73754
José Carlos Santiago <santiago.jose@outlook.com>
2018-11-18 14:03:01 -0600

    Initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { RestProvider } from '../../providers/rest/rest';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  users: any;
+  constructor(public navCtrl: NavController, public restProvider:RestProvider) {  
+ 
+  }
+
+  ionViewDidLoad(){
+    this.restProvider.obtenerdatos()
+    .subscribe(
+      (data)=>{this.users=data;},
+      (error)=>{console.log(error);}
+    )
+  }
+
+  
+
+}
---
+++ src/providers/rest/rest.ts
@@ -0,0 +1,31 @@
+import { HttpClient} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+
+
+
+/*
+  Generated class for the RestProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class RestProvider {
+
+  //apiUrl = 'https://jsonplaceholder.typicode.com';
+  apiUrl='http://localhost:59816/restienda/webresources/mypackage.';
+  constructor(public http: HttpClient) {
+    console.log('Hello RestProvider Provider');
+  }
+
+obtenerdatos(){
+  return this.http.get('http://localhost:1414/restful/usuarios.php');
+}
+
+obtenerusuario(idusuario){
+return this.http.get('http://localhost:1414/restful/usuarios.php/?idusuario='+idusuario);
+}
+
+}
